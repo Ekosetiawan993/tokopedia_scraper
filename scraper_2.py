21261605ee3ffeb374dcdef91d4e2855d0102db0
Eko Setiawan <ekomasterwan993@gmail.com>
2021-07-14 15:29:23 +0700

    Have an issue either it's lazy load or javascript problems, I will be back soon

--- scraper_2.py
+++ scraper_2.py
@@ -2,9 +2,10 @@ from requests_html import HTMLSession
 from bs4 import BeautifulSoup as bs
 
 BASE_URL = "https://www.tokopedia.com/gramedia-store/etalase/buku-remaja-dan-anak"
+url = "https://www.tokopedia.com/gramedia-store/etalase/buku-remaja-dan-anak?perpage=20"
 session = HTMLSession()
 
-page = session.get(BASE_URL)
+page = session.get(url)
 bs_page = bs(page.content, "html.parser")
 
 #block = page.html.find('div.css-tjjb18', first=True)
@@ -12,5 +13,9 @@ bs_page = bs(page.content, "html.parser")
 block = bs_page.find('div', class_='css-tjjb18')
 products = block.find_all('div', class_='css-1sn1xa2')
 
-print(len(products))
+juduls = page.html.find('div.css-1f4mp12')
 
+#print(len(products))
+print(len(juduls))
+for judul in juduls:
+    print(judul.text)
